f0b47120d5ffe5c2b03d088de7f04cdf54f5edc6
JuHaoChen1997 <juhaochen97@gmail.com>
2022-07-25 18:01:01 -0400

    add 404 page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Index from "./Pages/Index";
 import New from "./Pages/New";
 import Show from "./Pages/Show";
 import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/transactions/new" element={<New />} />
         <Route path="/transactions/:index/edit" element={<Edit />} />
         <Route path="/transactions/:index" element={<Show />} />
+        <Route path="*" element={<FourOFour />} />
       </Routes>
     </div>
   );
--- src/Pages/FourOFour.js
+++ src/Pages/FourOFour.js
@@ -1,5 +1,9 @@
 function FourOFour() {
-  return <></>;
+  return (
+    <div>
+      <h1>Not Found</h1>
+    </div>
+  );
 }
 
 export default FourOFour;
